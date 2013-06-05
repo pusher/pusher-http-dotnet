06cabbad199e736ab184eb761877645214cbe8d0
leggetter <phil@leggetter.co.uk>
2013-06-05 13:26:15 +0100

    delete old package files

--- package.cmd
+++ package.cmd
@@ -1,8 +1,9 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe pusher-dotnet-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 
-if not exist Download\package\lib\net35 mkdir Download\package\lib\net35\
+if exist Download\package rm -rf Download\package
+if not exist Download\package\lib\net35 mkdir Download\package\lib\net35\s
 
-copy README.md Download\Package\lib\net35\
+copy README.md Download\Package\
 
 copy PusherServer\bin\Release\PusherServer.dll Download\Package\lib\net35\
 
