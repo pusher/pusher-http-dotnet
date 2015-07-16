58bdecc589e2313f77ffedda885485812d7de75e
Mike Pye <mail@mdpye.co.uk>
2015-07-16 09:57:45 +0100

    Update version for release

--- PusherServer/Properties/AssemblyInfo.cs
+++ PusherServer/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-rc1")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc2")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PusherServer.Tests")]
