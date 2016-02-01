33d92ee533f27ef7d4f7a4f2bc6c7ab00bac7937
John McLoughlin <HowCanIHelp@BatfishSolutions.com>
2016-02-01 16:06:36 +0000

    PROD-165 Added XML comments to remove warnings during build

--- PusherServer/RequestResult.cs
+++ PusherServer/RequestResult.cs
@@ -4,11 +4,18 @@ using System;
 
 namespace PusherServer
 {
+    /// <summary>
+    /// Abstract base class for results coming back from request to the Pusher servers
+    /// </summary>
     public abstract class RequestResult: IRequestResult
     {
         string _body = null;
         private HttpStatusCode _statusCode;
 
+        /// <summary>
+        /// Constructor to constract the abstract base class for classes derived from RequestResults
+        /// </summary>
+        /// <param name="response"></param>
         public RequestResult(IRestResponse response)
         {
             if (response == null)
@@ -20,12 +27,18 @@ namespace PusherServer
             _statusCode = response.StatusCode;
         }
 
+        /// <summary>
+        /// Gets the Status Code returned in the wrapped Rest Response
+        /// </summary>
         public HttpStatusCode StatusCode
         {
             get { return _statusCode; }
             protected set { _statusCode = value; }
         }
 
+        /// <summary>
+        /// Gets the Body returned in the wrapped Rest Response
+        /// </summary>
         public string Body
         {
             get { return _body; }
