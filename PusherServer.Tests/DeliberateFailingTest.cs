be9ed9a4dc5508cd51c4bf8a095ecb0df88e0a41
John McLoughlin <HowCanIHelp@BatfishSolutions.com>
2016-02-03 12:24:04 +0000

    PROD-184 Testing failing test to make sure build fails

---
+++ PusherServer.Tests/DeliberateFailingTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PusherServer.Tests
+{
+    [TestFixture]
+    public class DeliberateFailingTest
+    {
+        [Test]
+        public void ThisTestWillFail()
+        {
+            Assert.AreEqual(1,2);
+        }
+    }
+}
