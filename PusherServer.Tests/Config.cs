f212555a60d6f386cd9560aba07fd60ab74997e0
John McLoughlin <HowCanIHelp@BatfishSolutions.com>
2016-02-03 11:05:11 +0000

    Prod-184 Changed Test config to use environment variables, instead of app.config

--- PusherServer.Tests/Config.cs
+++ PusherServer.Tests/Config.cs
@@ -1,12 +1,13 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 
 namespace PusherServer.Tests
 {
     internal static class Config
     {
-        private static string appId = ConfigurationManager.AppSettings.Get("pusher-app-id");
-        private static string appKey = ConfigurationManager.AppSettings.Get("pusher-app-key");
-        private static string appSecret = ConfigurationManager.AppSettings.Get("pusher-app-secret");
+        private static string appId = Environment.GetEnvironmentVariable("PUSHER_APP_ID");
+        private static string appKey = Environment.GetEnvironmentVariable("PUSHER_APP_KEY");
+        private static string appSecret = Environment.GetEnvironmentVariable("PUSHER_APP_SECRET");
 
         public static string AppId
         {
