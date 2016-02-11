fd9f28374b42410f373fa075f97220ecbac63f18
John McLoughlin <HowCanIHelp@BatfishSolutions.com>
2016-02-11 14:46:25 +0000

    Changelog, ReadMe & version updates for 3.0.0rc3

--- PusherServer/Properties/AssemblyInfo.cs
+++ PusherServer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pusher")]
 [assembly: AssemblyProduct("pusher-http-dotnet")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc3")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PusherServer.Tests")]
