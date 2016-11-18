d904e6c27df4f50d7528a6d0f14186e861a6004e
John McLoughlin <JohnMc@BatfishSolutions.com>
2016-11-18 14:34:20 +0000

    Core Update - Converted ASCII reference to UTF 8

--- PusherServer/CryptoHelper.cs
+++ PusherServer/CryptoHelper.cs
@@ -12,7 +12,7 @@ namespace PusherServer
 		{
             using (var md5 = MD5.Create())
             {
-                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(jsonData));
+                var result = md5.ComputeHash(Encoding.UTF8.GetBytes(jsonData));
                 return BytesToHex(result);
             }
 		}
