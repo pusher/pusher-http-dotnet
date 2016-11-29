ccbff33a0c5d629fba3558910c0905f7b2e57631
John McLoughlin <JohnMc@BatfishSolutions.com>
2016-11-29 20:48:35 +0000

    CoreUpdate - Revised build script

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build **/project.json
+
+dotnet restore
+dotnet build ./PusherServer.Core -c Release
+xbuild ./PusherServer/PusherServer.csproj
+xbuild ./PusherServer.Tests/PusherServer.Tests.csproj
+nunit-console PusherServer.Tests/bin/Release/PusherServer.Tests.dll
