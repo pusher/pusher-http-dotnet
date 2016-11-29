9ae44b3bac916728926331c8d8aa91b81b8919e4
John McLoughlin <JohnMc@BatfishSolutions.com>
2016-11-29 21:11:22 +0000

    CoreUpdate - Removed Core project

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@
 dotnet restore
 dotnet build ./PusherServer.Core -c Release
 xbuild ./PusherServer/PusherServer.csproj
-xbuild ./PusherServer.Tests/PusherServer.Tests.csproj
-nunit-console PusherServer.Tests/bin/Release/PusherServer.Tests.dll
