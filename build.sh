8d1090c53ad75e6ff5230c886d973907dc997531
John McLoughlin <JohnMc@BatfishSolutions.com>
2016-12-08 07:44:17 +0000

    CoreUpdate - Create .NET Core build shell script and travis file

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+dotnet restore ./PusherServer.Core/project.json
+
+# Instead, run directly with mono for the full .net version 
+dotnet build ./PusherServer.Core/project.json -c Release
