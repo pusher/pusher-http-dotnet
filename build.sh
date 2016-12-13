d3fa0105c5f70d08b734d66c04de774cbd96e61f
Hamilton Chapman <hamchapman@gmail.com>
2016-12-13 11:52:46 +0000

    Update build.sh and .travis.yml

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#exit if any command fails
+# exit if any command fails
 set -e
 
 dotnet restore ./PusherServer.Core/project.json
 
-# Instead, run directly with mono for the full .net version 
+# Instead, run directly with mono for the full .net version
 dotnet build ./PusherServer.Core/project.json -c Release
