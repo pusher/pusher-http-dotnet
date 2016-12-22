9210118badb37c1c8514ff671124ab3f93e5dab9
Sam Stagg <sam@pusher.com>
2016-12-22 13:34:47 +0000

    Changes to support new NuGet package
    - Updated nuget to latest version
    - Moved nuspec back into project folder so it can derive tokens from the
    PusherServer csproj
    - Updated AssemblyInfo to new details
    - Updated package.cmd to build the nuget package

--- package.cmd
+++ package.cmd
@@ -1,5 +1,5 @@
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\pusher-dotnet-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
-dotnet build .\PusherServer.Core
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\pusher-dotnet-everything-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 
 tools\nuget.exe update -self
-tools\nuget.exe pack .\PusherServer.nuspec
+tools\nuget.exe pack .\PusherServer\PusherServer.csproj -verbosity detailed -properties Configuration=Release
