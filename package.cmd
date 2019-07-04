5532c8b5bfa0ba3b46463b9aae3f6c5cbced30b3
John McLoughlin <JohnMc@BatfishSolutions.com>
2019-07-04 21:51:25 +0100

    Updated nuget package command to work with new projects

--- package.cmd
+++ package.cmd
@@ -1,5 +1,5 @@
 
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\pusher-dotnet-everything-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\msbuild.exe" .\pusher-dotnet-everything-server.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 
 tools\nuget.exe update -self
 tools\nuget.exe pack .\PusherServer\PusherServer.csproj -verbosity detailed -properties Configuration=Release
