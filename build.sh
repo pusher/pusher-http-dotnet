57ebc125595e2862005eb1a324afebe655d2cff9
Kevin <backupa76@gmail.com>
2019-07-05 14:58:51 +0100

    Retarget SDK to Standard 1.6 + Fix Travis Build. (#52)
    * Upgraded all .net core projects to .net standard 1.6
    
    Updated build.sh to point at the new project
    
    * Updated nuget package command to work with new projects
    
    * update CHANGELOG

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # exit if any command fails
 set -e
 
-dotnet restore ./PusherServer.Core/project.json
+dotnet restore ./PusherServer.Core/PusherServer.Core.csproj
 
 # Instead, run directly with mono for the full .net version
-dotnet build ./PusherServer.Core/project.json -c Release
+dotnet build ./PusherServer.Core/PusherServer.Core.csproj -c Release
