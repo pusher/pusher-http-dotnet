840045b3bcaac6ed3bc408e739dd55322b2ea3f8
Erwin Wendland <erwin@hotmail.co.uk>
2021-02-22 13:41:29 +0000

    Added exception classes ChannelNameFormatException, ChannelNameLengthExceededException and SocketIdFormatException.

--- PusherServer.Tests/UnitTests/Authenticate.cs
+++ PusherServer.Tests/UnitTests/Authenticate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using PusherServer.Exceptions;
 using PusherServer.Tests.Helpers;
 
 namespace PusherServer.Tests.UnitTests
@@ -29,83 +30,83 @@ namespace PusherServer.Tests.UnitTests
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_cannot_contain_colon_prefix()
         {
             _pusher.Authenticate("private-test", ":444.444");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_cannot_contain_colon_suffix()
         {
             _pusher.Authenticate("private-test", "444.444:");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_cannot_contain_letters_suffix()
         {
             _pusher.Authenticate("private-test", "444.444a");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_must_contain_a_period_point()
         {
             _pusher.Authenticate("private-test", "444");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_must_not_contain_newline_prefix()
         {
             _pusher.Authenticate("private-test", "\n444.444");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_must_not_contain_newline_suffix()
         {
             _pusher.Authenticate("private-test", "444.444\n");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(SocketIdFormatException))]
         public void socket_id_must_not_be_empty_string()
         {
             _pusher.Authenticate("private-test", string.Empty);
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(ChannelNameFormatException))]
         public void channel_must_not_have_trailing_colon()
         {
             AuthWithChannelName("private-channel:");
         }
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(ChannelNameFormatException))]
         public void channel_name_must_not_have_leading_colon()
         {
             AuthWithChannelName(":private-channel");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(ChannelNameFormatException))]
         public void channel_name_must_not_have_leading_colon_newline()
         {
             AuthWithChannelName(":\nprivate-channel");
         }
 
         [Test]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(ChannelNameFormatException))]
         public void channel_name_must_not_have_trailing_colon_newline()
         {
             AuthWithChannelName("private-channel\n:");
         }
 
         [Test]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [ExpectedException(typeof(ChannelNameLengthExceededException))]
         public void channel_names_must_not_exceed_allowed_length()
         {
             var channelName = new String('a', ValidationHelper.CHANNEL_NAME_MAX_LENGTH + 1);
