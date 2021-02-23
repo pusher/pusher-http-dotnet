1420ab5dbc6b7579f4a303335c6567a838917d0b
Erwin Wendland <erwin@hotmail.co.uk>
2021-02-23 13:35:58 +0000

    Added a new interface ITraceLogger and applied it as a property to IPusherOptions. Tracing is off by default. Added sample to readme file.

---
+++ PusherServer/Util/DebugTraceLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace PusherServer.Util
+{
+    /// <summary>
+    /// Traces debug messages using <see cref="System.Diagnostics.Debug"/>.
+    /// </summary>
+    public class DebugTraceLogger : ITraceLogger
+    {
+        /// <inheritdoc/>
+        public void Trace(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
